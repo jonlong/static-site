02be2b34f386e4b22e4bc81d4aef7541a66f9ea4
Jon Long <jon@linesandwaves.com>
2015-02-01 14:38:18 -0500

    fix watch paths

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -8,8 +8,8 @@ var config= require('../../config');
 var debug = require('gulp-debug');
 
 gulp.task('watch', ['setWatch', 'browserSync'], function() {
-  gulp.watch(config.paths.src.sass, ['sass']);
-  gulp.watch(config.paths.src.images, ['images']);
+  gulp.watch(config.paths.src.sass + '/**', ['sass']);
+  gulp.watch(config.paths.src.images + '/**', ['images']);
   gulp.watch(config.paths.src.templates + '/**', ['metalsmith']);
   gulp.watch(config.paths.src.base + '/**', ['metalsmith']);
 });
