b2f82bc68d2b10eaf53fa84535999bd966707ac2
Jon Long <jon@linesandwaves.com>
2015-02-16 15:11:10 -0500

    set up sample browserify build

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,12 @@
-module.exports = {};
+var $ = window.jQuery = require('../components/jquery/dist/jquery');
+
+require('../components/foundation/js/foundation/foundation');
+require('../components/foundation/js/foundation/foundation.reveal');
+
+module.exports = {
+  init: function() {
+    $(document).foundation();
+  }
+};
+
+module.exports.init();
