72f2a80ee98e20dd15e953a17cf1e1d25d0fdcdc
Jon Long <jon@linesandwaves.com>
2015-02-16 14:15:44 -0500

    clean up sample templates

--- gulp/tasks/metalsmith.js
+++ gulp/tasks/metalsmith.js
@@ -19,25 +19,13 @@ var swigExtras = require('swig-extras');
 var config = require('../../config');
 
 var NODE_ENV = process.env.NODE_ENV || 'development';
-var BASE_URL = process.env.BASE_URL || config.blog.baseURL;
-
-var buildResponsiveImageMarkup = function(params) {
-  return '<picture>\
-              <!--[if IE 9]><video style="display: none;"><![endif]-->\
-              <source srcset="' + config.paths.build.images + '/' + params.lg + '" media="(min-width: 1000px)">\
-              <source srcset="' + config.paths.build.images + '/' + params.md + '" media="(min-width: 800px)">\
-              <!--[if IE 9]></video><![endif]-->\
-              <img srcset="' + config.paths.build.images + '/' + params.sm + '" alt="' + params.alt + '">\
-          </picture>';
-};
+var BASE_URL = process.env.BASE_URL || config.site.baseURL;
 
 var buildCaptionedImageMarkup = function(params) {
-  var image = buildResponsiveImageMarkup(params);
-
-  return '<figure class="media">\
-              ' + image + '\
-              <figcaption class="media__caption">' + params.caption + '</figcaption>\
-          </figure>';
+  return '<div class="captioned-image">\
+              <img class="img-responsive" src="' + params.src + '" />\
+              <p class="caption">' + params.caption + '</p>\
+          </div>';
 };
 
 /**
@@ -77,7 +65,7 @@ gulp.task('metalsmith', function() {
           env: NODE_ENV,
           BASE_URL: BASE_URL,
           now: new Date(),
-          blog: config.blog
+          site: config.site
         })
         // Use shortcodes for custom markdown tags
         .use(shortcodes({
@@ -85,10 +73,6 @@ gulp.task('metalsmith', function() {
               'captioned-image': function(str, params) {
                 var image = buildCaptionedImageMarkup(params);
                 return image;
-            },
-            'image': function(str, params) {
-              var image = buildResponsiveImageMarkup(params);
-              return image;
             }
           }
         }))
@@ -98,7 +82,9 @@ gulp.task('metalsmith', function() {
         }))
         .use(collections({
           posts: {
-            pattern: 'posts/*'
+            pattern: 'posts/*',
+            sortBy: 'date',
+            reverse: true
           }
         }))
         .use(permalinks({
---
+++ src/posts/sample-post-2.md
@@ -0,0 +1,16 @@
+---
+title: Sample Article 2
+date: 2014-12-23
+template: post.swig
+author: A. Person
+lead: Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation [ullamco laboris]() nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+shortcodes: true
+---
+
+[captioned-image src="http://placehold.it/800x350" alt="Image: Placeholder" caption="Here's a placeholder image"]
+
+**Lorem ipsum dolor sit amet**, consectetur adipisicing elit. Consequatur a, ullam, voluptatum incidunt neque doloremque vel inventore distinctio [laudantium harum]() illo quam nulla dolor alias iure impedit! Accusamus! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur, a, ullam, voluptatum incidunt neque porro numquam doloremque vel inventore distinctio laudantium harum illo quam nulla dolor alias iure impedit.
+
+## Subheading lorem
+
+**Consequatur ullam, voluptatum** incidunt neque porro numquam doloremque vel inventore distinctio laudantium harum illo quam nulla dolor alias iure impedit. Accusamus. Consequatur, a, ullam, voluptatum incidunt neque porro numquam doloremque vel inventore distinctio laudantium harum illo quam nulla dolor alias iure impedit! Accusamus.
